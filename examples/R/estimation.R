f8b59355cad9f79c844b40048e9f5d23ec3610e4
Tillmann Nett <mail@tcrueger.de>
2020-08-27 15:42:58 +0200

    Updated estimation script for PCS

--- examples/R/estimation.R
+++ examples/R/estimation.R
@@ -5,6 +5,8 @@ rm(list = ls())
 library(R2jags)
 library(tidyverse)
 
+load.module("Pdp")
+
 # set max.print to something very large, so we can get
 # the full results
 options(max.print=1000000)
@@ -17,8 +19,8 @@ get.samples <- function(mcmc,var,...) {
   return( mcmc[,var] )
 }
 
-n.burn   <-  5000
-n.iter   <- 20000
+n.burn   <-   5000
+n.iter   <-  20000
 n.thin   <-     8
 n.chains <-     8
 
@@ -34,10 +36,13 @@ jData <- list(
   choices    = df.choices$optionAchosen
 )
 
+start <- Sys.time()
 jres <- jags.parallel(data=jData, model.file = "estimation.bugs", parameters.to.save = c("f","p","c","lambda"),
                       n.iter = n.iter, n.burnin = n.burn, n.thin = n.thin, n.chains = n.chains,
                       export_obj_names=c("n.iter","n.burn","n.thin","n.chains"),
                       jags.module = c("glm","dic","Pdp"))
 jres
+end <- Sys.time()
+print(end - start)
 
 samples <- jres %>% as.mcmc() %>% as.matrix()
